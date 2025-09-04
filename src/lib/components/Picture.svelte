afe15b9de1e21f423d54502934a77db8e4dcebee
ileies <admin@ileies.de>
2025-09-04 17:46:08 +0200

    Implemented Picture component

---
+++ src/lib/components/Picture.svelte
@@ -0,0 +1,15 @@
+<script lang="ts">
+	const { src, alt, w, h }: { src: string, alt: string, w: number, h: number } = $props();
+</script>
+
+<div 
+	class="relative overflow-hidden rounded-md" 
+	style="width: {w}px; height: {h}px;"
+>
+	<img 
+		{src} 
+		{alt} 
+		loading="lazy" 
+		class="absolute inset-0 w-full h-full object-cover"
+	/>
+</div>
