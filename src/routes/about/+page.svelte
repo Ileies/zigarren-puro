0e729a02805056bcb98053390c85c7f85cc0e6d2
ileies <admin@ileies.de>
2025-09-04 17:49:02 +0200

    Added about us route with maps view

---
+++ src/routes/about/+page.svelte
@@ -0,0 +1,4 @@
+<script>
+</script>
+
+<iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3250.128384566315!2d6.952829677051009!3d50.947863351424324!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x47bf254366f7cc2f%3A0x2c0fcbe2eb7a7cc7!2sZigarren%20Puro!5e1!3m2!1sen!2sde!4v1756668284711!5m2!1sen!2sde" width="600" height="450" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
